10b155c7c1314ce21b5984b06fcf8fe66f4e0555
Benjannabis <51462063+Benjannabis@users.noreply.github.com>
2019-08-04 23:23:50 -0400

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,10 @@
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+
+description 'Bailes - Benjamin Cortez'
+
+
+client_script "@NativeUI/NativeUI.lua"
+
+client_scripts {
+	'client.lua',
+}
